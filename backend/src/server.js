ea9ca474fdf4125d744d78b1b713cf7ea12ac67d
Kavya <kavya01092003@gmail.com>
2025-08-19 15:18:01 +0530

    final build

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,13 @@ import chatRoutes from "./routes/chat.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -25,6 +27,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 //console.log("Routes loaded");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
